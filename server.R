9cc85357b3da4140c885aad1a8a986fc5df70aaf
M <41757651+GMY2018@users.noreply.github.com>
2019-10-18 10:17:45 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -1,3 +1,6 @@
+## -----------------------------------------------
+## Live demo of data analysis (server function)
+## -----------------------------------------------
 
 library(shiny)
 library(leaflet)
@@ -9,34 +12,31 @@ library(stringr)
 library(rdrop2)
 
 
-## set the directory for saving and reading data
-# outputDir <- "update"  # for local use
 
-token <- readRDS("dropbox_token.rds")
-drop_acc(dtoken = token)
-outputDir <- "OpenDay"   # for DropBox access
+## -------------------------------------------
+## Get the essential matrices, vectors, etc
+## -------------------------------------------
+
+## set the directory for saving and reading data
+outputDir <- "update"  # for local use
+
+
+## load the volunteer data
+basedata.ini <- readRDS("basedata0.rds")  # for testing
+basedata.ini <- basedata.ini[, -4]
+
 
 ## load the postcode data and CF factors
 post2loc <- readRDS("partialpost.rds")
 CF.vec <- readRDS("CFfactor.rds")
 
-## load the volunteer data
-basedata.ini <- readRDS("basedata0.rds")  # for testing
-# basedata.ini <- readRDS(file.path(outputDir, "basedata.rds"))
-basedata.ini <- basedata.ini[, -4]
 
-## load the data from Dropbox (this will run everytime we relaunch the app)
-filesInfo <- drop_dir(outputDir)
-if (length(filesInfo) > 0) {
-  filePaths <- filesInfo$path_display
-  dropdata <- lapply(filePaths, drop_read_csv)   # stringsAsFactors = FALSE
-  dropdata <- do.call(rbind, dropdata)[, -1]
-  
-  # merge the two (this is the most up-to-date version of data)
-  basedata.ini <- rbind(basedata.ini, dropdata)
-}
+## define an object for storing reactive values
+basedata <- reactiveValues()   
+basedata$mat <- basedata.ini
 
-# get the coordinates for the curves on the map
+
+## get the coordinates for the curves on the map
 lon.ini <- lat.ini <- 1:nrow(basedata.ini)
 for (i in 1:nrow(basedata.ini)) {
   id <- post2loc$postcode4 %in% basedata.ini$postcode[i]
@@ -46,7 +46,8 @@ for (i in 1:nrow(basedata.ini)) {
   lat.ini[i] <- lat1
 }
 
-# mycolor <- c("skyblue4", "skyblue3", "darkolivegreen3", "darkolivegreen4", "goldenrod1", "tan1")
+
+## set a few nice colors
 mycolor <- c("#195D94", "#5186B1", "#548E1B", "#94C622", "#FFC300", "#EE6D2D")
 # slategray (light): #859AB8
 # slategray (dark): #29528E
@@ -54,27 +55,20 @@ mycolor <- c("#195D94", "#5186B1", "#548E1B", "#94C622", "#FFC300", "#EE6D2D")
 # gray: #505050
 # white: #FFFFFF
 # brown3: #A5151E
-  
-## set Google API
+
+
+## set Google API (you might wish to register for your own)
 set.api.key("AIzaSyA6051uiw89Uxx3RqmS-Fzqrmfyn-pA4-Q")
 
-## define an object for storing reactive values
-basedata <- reactiveValues()   
-basedata$mat <- basedata.ini
-
-fileName <- reactiveValues()
-fileName$name <- NULL
 
 
-## App functions
+## -------------------------------------------------------
+## The app functions (for everything on the ui.R script)
+## -------------------------------------------------------
+
 function(input, output, session) {
   
   ## (1) The map output
-  # map0 <- leaflet() %>%   
-  #   addTiles(urlTemplate = "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>%
-  #   setView(lng=-2.8556, lat=54.4969, zoom=5)
-  # 
-  # output$basemapUK <- leaflet::renderLeaflet(map0)
   
   output$basemapUK <- renderLeaflet({
     map0 <- leaflet() %>%
@@ -94,27 +88,30 @@ function(input, output, session) {
   
   
   ## (2) add popups and curves when new visitor comes
+  
   visitor <- eventReactive(input$update, {
     post4 <- input$post4
     # formatting the post code
-    post4 <- str_trim(post4, side="both")  # remove spaces at two ends
-    nd <- str_count(post4)  # count the digits
+    # remove spaces at two ends
+    post4 <- str_trim(post4, side="both")  
+    # count the digits
+    nd <- str_count(post4)
+    # insert space if it's missing, then remove duplicated spaces in the middle
     part1 <- substr(post4, start=1, stop=nd-1)
     part2 <- substr(post4, start=nd, stop=nd)
-    post4 <- str_squish(paste(part1, part2))
-    # insert space if there is nont, then remove duplicated spaces in the middle
-    post4 <- toupper(post4)  # to upper case
+    post4 <- str_squish(paste(part1, part2)) 
+    # convert to upper case
+    post4 <- toupper(post4)  # good to go!
+    
     travel4 <- input$travel
     num4 <- as.numeric(input$groupN)
+    
     list(post4, travel4, num4)
   })
   
   observeEvent(input$update, {
     post4 <- visitor()[[1]]
-    # id4 <- post2loc$postcode4 %in% post4
     id4 <- match(post4, post2loc$postcode4)
-    
-    # validate(need(!is.na(id4), 'Invalid postcode, please double check!'))
     
     if (!is.na(id4)) {
       lon4 <- post2loc$med_lon[id4]
@@ -134,23 +131,20 @@ function(input, output, session) {
       leafletProxy("basemapUK") %>%
         leaflet::addPopups(lon4, lat4, content)
     } else {
-      id <<- showNotification("Warning: postcode not valid!", type="error", duration=10)
-      # this is too small, and it appeared in the corner...
+      id <<- showNotification("Warning: postcode not valid!", type="warning", duration=10)
+      # This is too small, and it is in the corner. How to change this?
     }
    
   })
   
   
   ## (3) Update the data set for new analysis
-  # basedata <- reactiveValues()   # returns an object for storing reactive values
-  # basedata$mat <- basedata.ini
   
   observeEvent(input$update, {
     post4 <- visitor()[[1]]
     transp <- visitor()[[2]]
     gn <- visitor()[[3]]
     
-    # id4 <- post2loc$postcode4 %in% post4
     id4 <- match(post4, post2loc$postcode4)
     if (!is.na(id4)) {
       lon4 <- post2loc$med_lon[id4]
@@ -163,32 +157,20 @@ function(input, output, session) {
       if (transp=="car") {transp0 <- "driving"}
       dist4 <- gmapsdistance(origin = paste(lat4, lon4, sep='+'), 
                              destination = "52.87352+-1.08628", 
-                             mode = paste(transp0), 
-                             dep_date="2019-10-19", dep_time="08:00:00")
-      dist4m <- round(dist4$Distance/1600, 2)  # convert to miles, keep 3 decimal places
-      if (dist4m == 0) {dist4m <- 0.5}
+                             mode = paste(transp0),
+                             dep_date = as.character(Sys.Date()+1), 
+                             dep_time = "09:00:00")
+      dist4m <- round(dist4$Distance/1609.34, 3)  # convert to miles, keep 3 decimal places
+      if (dist4m == 0) {dist4m <- 0.5}  # if you live in Keyworth, I assume you travelled 0.5 miles
       
       CF.new <- dist4m * CF.vec[match(transp, names(CF.vec))] / 1000
       if (transp == "car") {CF.new <- CF.new/gn}
       CF.new <- round(CF.new, 3)
       
-      # update the database
       adddata <- data.frame(postcode=post4, transport=transp, group_of=gn, 
                             distance=dist4m, footprint=CF.new)
       basedata$mat <- rbind(basedata$mat, adddata)
-      
-      # maybe I shouldn't adding things here, just in case it gets messed up?
-      # dropfilesInfo <- drop_dir(outputDir)
-      # dropfilePaths <- dropfilesInfo$path_display
-      # dropdata <- lapply(filePaths, drop_read_csv)   # stringsAsFactors = FALSE
-      # dropdata <- do.call(rbind, dropdata)[, -1]
-      
-      # save the new data to DropBox
-      fileName$name <- paste0(as.integer(Sys.time()), ".csv")
-      filePath <- file.path(tempdir(), fileName$name)
-      write.csv(adddata, file=filePath)
-      drop_upload(filePath, path=outputDir)
-      
+  
     } else {
       basedata$mat <- basedata$mat
     }
@@ -197,30 +179,25 @@ function(input, output, session) {
     output$newdat <- renderDataTable(basedata$mat[nrow(basedata$mat):1, ])
     # if postcode correct, this will be the new table, 
     # if not, this will be the old table
+    
+    # save the data table every 5 new visitors, so we won't lose information
+    if ((nrow(basedata$mat) %% 5) == 0) {
+      fileName <- paste0("basedata", ".rds")
+      saveRDS(basedata$mat, file=file.path(outputDir, fileName))
+    }
+    
   })
-  
-  # The following code has the same effect
-  # output$newdat <- renderDataTable({
-  #   post4 <- visitor()[[1]]
-  #   transp <- visitor()[[2]]
-  #   gn <- visitor()[[3]]
-  #   adddata <- data.frame(postcode=post4, transport=transp, group_of=gn)
-  #   isolate(basedata$mat <- rbind(basedata$mat, adddata))
-  #   basedata$mat
-  # })
-  
+
   
   ## (4) Basic statistical analysis
   
   output$piechart <- renderPlot({
     if (input$pie) {
-      # methodmat <- basedata$mat[, 2:3]
       pie.tab <- tapply(as.numeric(basedata$mat[,3]), 
                         INDEX=basedata$mat[,2], FUN=sum)
       par(mfrow=c(1, 1), mar=c(3, 3, 2.5, 2.5))
       pie(pie.tab, labels=paste0(names(pie.tab), " - ", round(100*pie.tab/sum(pie.tab), 1), "%"), 
           edge=100, radius=0.8, col=mycolor, border="#FFFFFF", init.angle=45, cex.lab=1)
-      # try associate one colour to one transportation method
     }
   })
   
@@ -238,13 +215,13 @@ function(input, output, session) {
       par(mfrow=c(1, 1), mar=c(4, 4.5, 2.5, 2.5)+0.5, fg="#FFFFFF")
       boxplot(basedata$mat$distance ~ basedata$mat$transport, 
               horizontal=FALSE, col="#859AB8", border="#29528E",
-              ylab="miles", xlab="", # main='Transportation methods and distances', 
+              ylab="miles", xlab="", 
               cex.lab=1, cex.axis=1)
     }
   })
   
   
-  ## (5) Calculate carbon footprint related
+  ## (5) Calculate carbon footprint and make a histogram
   output$carbon <- renderText({
     CF.dat <- basedata$mat$footprint[nrow(basedata$mat)]
     paste("The carbon footprint of your journey to the BGS Open Day is",
@@ -253,12 +230,15 @@ function(input, output, session) {
   
   
   observeEvent(input$carbonD, {
-    hist.data <- rep(basedata$mat$footprint, times=basedata$mat$group_of)
-    your.data <- basedata$mat$footprint[nrow(basedata$mat)]
+    isolate({
+      # so that the information is only updated when you click the bottom
+      hist.data <- rep(basedata$mat$footprint, times=basedata$mat$group_of)
+      your.data <- basedata$mat$footprint[nrow(basedata$mat)]
+    })
     output$cfdistribution <- renderPlot({
       par(mfrow=c(1,1), mar=c(4.5, 4.5, 3, 2)+0.5)
       hist(hist.data, probability=T, breaks=20, col="#859AB8", border="#FFFFFF",
-           xlab="kg carbon dioxide", ylab="probability", 
+           xlab="kg carbon dioxide / person", ylab="probability", 
            main="Distribution of carbon footprint",
            col.main="#505050", col.axis="#505050", cex.lab=1.1, cex.main=1.2)
       abline(v=your.data, lwd=2, lty=2, col="#A5151E")
@@ -266,23 +246,8 @@ function(input, output, session) {
     })
   })
   
-  # output$cfdistribution <- renderPlot({
-    # if (input$carbonD) {
-    #   hist.data <- rep(basedata$mat$footprint, times=basedata$mat$group_of)
-    #   par(mfrow=c(1,1), mar=c(4.5, 4.5, 3, 2)+0.5)
-    #   hist(hist.data, probability=T, breaks=20, col='slategray3', border='white', 
-    #        xlab='kg CO2 / person', ylab='probability', main='Distribution of carbon footprint',
-    #        col.main='gray30', col.axis='gray30', cex.lab=1.1, cex.main=1.2)
-    #   abline(v=basedata$mat$footprint[nrow(basedata$mat)], lwd=2, lty=2, col='brown3')
-    #   points(x=basedata$mat$footprint[nrow(basedata$mat)], y=0, pch=17, lwd=3, col='brown3')
-    #   
-    #   # sm.density <- density(hist.data, bw=3, kernel='gaussian')
-    #   # lines(sm.density$x, sm.density$y, lwd=3, col='slategray')
-    # }
-  # })
   
-  
-  ## (6) Some fun facts
+  ## (6) The fun fact
   
   output$earth <- renderImage({
     imageName <- normalizePath(file.path("./www", "earth.png"))
@@ -300,60 +265,30 @@ function(input, output, session) {
            alt = "Image not found")
       # }
     }, deleteFile = FALSE)
-  })
-  
-  observeEvent(input$funfact,  {
-    output$speed <- renderText({
-      # earch.sp1 <- cos(52/180*pi) * 1674.4 / 3.6  # UK speed (m/s)
-      earch.sp2 <- cos(0/180*pi) * 1674.4 / 3.6  # equator speed (m/s)
+    
+    isolate({
       dist <- basedata$mat$distance[nrow(basedata$mat)]    
-      earth.t <- (dist * 1609.34 / earch.sp2) / 60  # time in minutes
+    })
+    
+    output$speed <- renderText({
+      # 1674.4 km/h (source: Wikipaedia)
+      earch.sp1 <- cos(52/180*pi) * 1674.4 / 3.6  # UK speed (m/s)
+      # earch.sp2 <- cos(0/180*pi) * 1674.4 / 3.6  # equator speed (m/s)
+      earth.t <- (dist * 1609.34 / earch.sp1) / 60  # time in minutes
       earth.t <- round(earth.t, 3)
-      
       paste0("It may take you quite a while to get here, but it only takes the earth ", earth.t, 
              " minutes to rotate the distance (that's about ", dist, " miles) you travelled today! ",
              "Yet you barely notice it's moving.")
     })
   })
-    
   
   
-  ## (7) The reset button!
+  ## (7) The reset button (for remove the last record if incorrectly submitted)
+  
   observeEvent(input$reset, {
-    last.post <- basedata$mat$postcode[nrow(basedata$mat)]
     basedata$mat <- basedata$mat[-nrow(basedata$mat), ]
-    # HOW TO remove the curve on the map
-    
-    # also need to delete the corresponding file on Dropbox (need reactive)
     drop_delete(file.path(outputDir, fileName$name))
   })
 
-## The end
 }
-
-
-## save and reload data (save one record a time)
-
-# outputDir <- "responses"
-# 
-# saveData <- function(data) {
-#   data <- t(data)
-#   # Create a unique file name
-#   fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
-#   # Write the data to a temporary file locally
-#   filePath <- file.path(tempdir(), fileName)
-#   write.csv(data, filePath, row.names = FALSE, quote = TRUE)
-#   # Upload the file to Dropbox
-#   drop_upload(filePath, path = outputDir)
-# }
-# 
-# loadData <- function() {
-#   # Read all the files into a list
-#   filesInfo <- drop_dir(outputDir)
-#   filePaths <- filesInfo$path   # not quite right...
-#   data <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE)
-#   # Concatenate all data together into one data.frame
-#   data <- do.call(rbind, data)
-#   data
-# }
 
--- ui.R
+++ ui.R
@@ -1,12 +1,21 @@
+## -----------------------------------------------
+## Live demo of data analysis (user interface)
+## -----------------------------------------------
 
 library(shiny)
 library(leaflet)
 library(maps)
 
 
+
+## -----------------------
+## The user interface
+## -----------------------
+
 fluidPage(
   
   ## (1) The header and the data input section
+  
   fluidRow(
     column(9, offset=1,
            titlePanel("Live demo of data analysis", windowTitle="Live demo"))
@@ -16,13 +25,9 @@ fluidPage(
     column(4, offset=1,
            br(),
            textInput(inputId="post4", label="Type in partial postcode (omitting last 2 digits)", value=NULL, placeholder="NG12 5"),
-           strong("For example, if the postcode is 'NG12 5GG', type 'NG12 5'.", style = "color:gray"),
+           strong("For example, if the postcode is 'NG12 5GG', type 'NG12 5'.", style = "color:#505050"),
            br(),
            br(),
-           # radioButtons(inputId='travel', label='Please select the transportation method', 
-           #              choices=c('Walk'='walk', 'Cycle'='cycle', 'Car'='car',
-           #                        'Bus'='bus', 'Train'='train', 'Plane'='plane'),
-           #              selected='walk'),
            
            radioButtons(inputId="travel", label="Select a transportation method",
                         choiceNames=list(list(icon("walking"), "Walking"), 
@@ -42,8 +47,7 @@ fluidPage(
            column(3, actionButton(inputId="reset", label="Reset", icon=icon("undo"))),
            br(),
            br(),
-           br(),
-           strong("Note: After clicking the 'Submit' button, you will see your record on the first row of the table.
+           h6("Note: After clicking the 'Submit' button, you will see your record on the first row of the table.
                If you see a warning sign popped out at the bottom right corner, 
                just close it and type in the correct partial postcode again. 
                Only use the 'Reset' button when you accidentially put in someone else's postcode!",
@@ -54,51 +58,57 @@ fluidPage(
     
     column(6, offset=1,
            br(),
-           # a map, with pop ups indicating the locations
-           leafletOutput("basemapUK", width="85%"),
+           leafletOutput("basemapUK", width="80%"),
            br(),
            br()
            )
+    
+  ## end of the header (fluid row)
   ),
   
   
   ## (2) A multiple panels section for simple data analysis
+  
   fluidRow(
     column(1),
     
     column(10,
            tabsetPanel(
              
+             ## The 1st panel
              tabPanel("Visitors' survey",  
                       column(1),
                       column(10, dataTableOutput("newdat")),
                       column(1)),
              
              
+             ## The 2nd panel
              tabPanel("Data analysis",
                       column(2,
                              br(),
                              h4(icon("chart-line"), "Some basic statistical analysis"),
                              br(),
-                             checkboxInput("pie", strong("Make a piechart", style = "color:gray"), FALSE),
-                             checkboxInput("box", strong("Make a boxplot", style = "color:gray"), FALSE),
+                             checkboxInput("pie", strong("Make a piechart", style = "color:#505050"), FALSE),
+                             checkboxInput("box", strong("Make a boxplot", style = "color:#505050"), FALSE),
                              br(),
                              br()),
                       
                       column(5,
                              br(),
-                             h5(strong("Pie chart: proportions of usage of different transportation methods"), style = "color:gray"),
+                             h5(strong("Pie chart: proportions of usage of different transportation methods"), style = "color:#505050"),
                              plotOutput("piechart"),
-                             h5(textOutput("carshare"), style = "color:gray"),
+                             h5(textOutput("carshare"), style = "color:#505050"),
                              br()),
                       
                       column(5,
                              br(),
-                             h5(strong("Boxplot: distribution of distance by transportation methods"), style = "color:gray"),
+                             h5(strong("Boxplot: distribution of distance by transportation methods"), style = "color:#505050"),
                              plotOutput("boxplots"),
                              br()
                              )),
              
+             
+             ## The 3rd panel
              tabPanel("Carbon footprint",  
                       column(3, 
                              br(),
@@ -106,14 +116,14 @@ fluidPage(
                                 icon("shoe-prints"), icon("shoe-prints"), icon("shoe-prints")),
                              h4(textOutput('carbon')),
                              br(),
-                             h5("Want to see how well you did today?"),
+                             h4("Click to see some personalized data", style = "color:#505050"),
+                             br(), 
+                             h5("How well you did today?"),
                              actionButton('carbonD', label='You are here', icon=icon("flag-checkered")),
-                             # checkboxInput('carbonD', 
-                             #               strong('You in the distribution', style = 'color:gray'), FALSE),
                              br(),
                              br(),
                              h5("Do you know how fast the earth spin?"),
-                             actionButton("funfact", label="Fun fact", icon=icon("rocket")),
+                             actionButton("funfact", label="You v.s the Earth", icon=icon("rocket")),
                              br(),
                              br(),
                              imageOutput("earth"),
@@ -131,24 +141,12 @@ fluidPage(
                              br())
                       )
              
-             # tabPanel("Evolution history",  
-             #          column(2, 
-             #                 br(),
-             #                 br(),
-             #                 actionButton("play", label='Play', icon=icon("chevron-circle-right")),
-             #                 br(),
-             #                 br(),
-             #                 actionButton("stop", label='Stop', icon=icon("stop-circle")),
-             #                 br()),
-             #          column(5, plotOutput("pieTS")),
-             #          column(5, plotOutput("histTS"))
-             #          )
-             
-           # end of tabsetPanel
+           ## end of tabsetPanel
            )),
     
     column(1)
   )
+  
   
 ## end of fluid page
 )
